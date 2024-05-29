ee1d3fcf9f710fc9c238a1830e07f7858a046499
LeandroBiloni <leandrobiloni@hotmail.com>
2024-05-29 10:55:24 -0300

    Add Login tests

---
+++ src/pages/LoginPage.ts
@@ -0,0 +1,58 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/BasePage";
+import dotenv from 'dotenv'; 
+import { URLAssembler } from "../helpers/URLAssembler";
+dotenv.config();
+
+/**
+ * Class for Login Page.
+ */
+export class LoginPage extends BasePage{
+
+    private emailField: Locator;
+    private passwordField: Locator;
+    private loginButton: Locator;
+
+    /**
+     * Class constructor.
+     * @param page 
+     */
+    constructor(page: Page) {
+        super(page);
+        const baseURl = process.env.BASE_URL as string;
+        this.pageRoute = "login";
+        this.pageCompleteURL = URLAssembler.getAssembledURL(baseURl, this.pageRoute);
+
+        this.emailField = this.page.locator("[id='email']");
+        this.passwordField = this.page.locator("[id='password']");
+        this.loginButton = this.page.locator("[class='btn-login']");
+    }
+
+    async inputEmail(email: string): Promise<void> {
+        this.logger.info(`Writing email: ${email}`);
+        await this.emailField.waitFor({state: "attached"});
+        await this.emailField.fill(email);
+    }
+
+    async inputPassword(password: string): Promise<void> {
+        this.logger.info(`Writing password: ${password}`);
+        await this.passwordField.waitFor({state: "attached"});
+        await this.passwordField.fill(password);
+    }
+
+    async clickLogin(): Promise<void> {
+        this.logger.info("Clicking Login button");
+        await this.loginButton.waitFor({state: "attached"});
+        await this.loginButton.click();
+    }
+
+    async doLoginProcess(email: string, password: string): Promise<void> {
+        this.logger.info("Doing Login process");
+
+        this.inputEmail(email);
+
+        this.inputPassword(password);
+
+        this.clickLogin();
+    }
+}
---
+++ src/tests/LoginTests.spec.ts
@@ -0,0 +1,194 @@
+import test, { expect } from "@playwright/test";
+import { HomePage } from "../pages/HomePage";
+import { OrdersPage } from "../pages/OrdersPage";
+import { Header } from "../components/Header";
+import { ClientsPage } from "../pages/ClientsPage";
+import { ProductsPage } from "../pages/ProductsPage";
+import { allure } from "allure-playwright";
+import { Screenshoter } from "../helpers/Screenshoter";
+import { LoginPage } from "../pages/LoginPage";
+
+test.afterEach(async ({ page }, testInfo) => {
+    if (testInfo.status === "failed") {
+        const addTimeStamp = true;
+        await Screenshoter.TakeScreenshot(page, testInfo.title, addTimeStamp);
+    }
+});
+
+test.describe('Login Page Tests', {tag: ['@login-page', '@full-regression', '@smoke']}, () => {
+    test('Correct Login', async ({ page }) => {
+        
+        //Arrange
+        const email = process.env.CORRECT_EMAIL as string;
+        const password = process.env.CORRECT_PASSWORD as string;
+
+        const expectedDialog = "exito";
+        let currentDialog = "";
+
+        page.on('dialog', async (dialog) => {
+            currentDialog = dialog.message();
+            console.log("At dialogue popup: " + currentDialog);
+            await dialog.accept();
+        });
+
+        //Act - Do login
+        const loginPage = new LoginPage(page);
+        await allure.step("Step 1 - Go to Login Page", async () => {
+            await page.goto(loginPage.getURL());
+        });
+
+        await allure.step("Step 2 - Input email", async () => {
+            await loginPage.inputEmail(email);
+        });
+
+        await allure.step("Step 3 - Input password", async () => {
+            await loginPage.inputPassword(password);
+        });
+
+        await allure.step("Step 4 - Click Login button", async () => {
+            await loginPage.clickLogin();
+        });
+
+        await page.waitForEvent('dialog');
+
+        //Assert
+        await expect(currentDialog).toBe(expectedDialog);
+    });
+
+    test('Wrong Login - Empty fields', async ({ page }) => {
+        
+        //Arrange
+        const expectedDialog = "noup";
+        let currentDialog = "";
+
+        page.on('dialog', async (dialog) => {
+            currentDialog = dialog.message();
+            console.log("At dialogue popup: " + currentDialog);
+            await dialog.accept();
+        });
+
+        //Act - Do login
+        const loginPage = new LoginPage(page);
+        await allure.step("Step 1 - Go to Login Page", async () => {
+            await page.goto(loginPage.getURL());
+        });
+
+        await allure.step("Step 2 - Click Login button", async () => {
+            loginPage.clickLogin();
+        });
+
+        await page.waitForEvent('dialog');
+
+        //Assert        
+        await expect(currentDialog).toBe(expectedDialog);
+    });
+
+    test('Wrong Login - Empty email', async ({ page }) => {
+        
+        //Arrange
+        const password = "asd";
+
+        const expectedDialog = "noup";
+        let currentDialog = "";
+
+        page.on('dialog', async (dialog) => {
+            currentDialog = dialog.message();
+            console.log("At dialogue popup: " + currentDialog);
+            await dialog.accept();
+        });
+
+
+        //Act - Do login
+        const loginPage = new LoginPage(page);
+        await allure.step("Step 1 - Go to Login Page", async () => {
+            await page.goto(loginPage.getURL());
+        });
+
+        await allure.step("Step 2 - Input password", async () => {
+            loginPage.inputPassword(password);
+        });
+
+        await allure.step("Step 3 - Click Login button", async () => {
+            loginPage.clickLogin();
+        });
+
+        await page.waitForEvent('dialog');
+
+        //Assert        
+        await expect(currentDialog).toBe(expectedDialog);
+    });
+
+    test('Wrong Login - Empty password', async ({ page }) => {
+        
+        //Arrange
+        const email = "asd@asd";
+
+        const expectedDialog = "noup";
+        let currentDialog = "";
+
+        page.on('dialog', async (dialog) => {
+            currentDialog = dialog.message();
+            console.log("At dialogue popup: " + currentDialog);
+            await dialog.accept();
+        });
+
+        //Act - Do login
+        const loginPage = new LoginPage(page);
+        await allure.step("Step 1 - Go to Login Page", async () => {
+            await page.goto(loginPage.getURL());
+        });
+
+        await allure.step("Step 2 - Input email", async () => {
+            loginPage.inputEmail(email);
+        });
+
+        await allure.step("Step 3 - Click Login button", async () => {
+            loginPage.clickLogin();
+        });
+
+        await page.waitForEvent('dialog');
+
+        //Assert        
+        await expect(currentDialog).toBe(expectedDialog);
+    });
+
+    test('Wrong Login - Wrong credentials', async ({ page }) => {
+        
+        //Arrange
+        const email = "asd@asd";
+        const password = "asd";
+
+        const expectedDialog = "noup";
+        let currentDialog = "";
+
+        page.on('dialog', async (dialog) => {
+            currentDialog = dialog.message();
+            console.log("At dialogue popup: " + currentDialog);
+            await dialog.accept();
+        });
+
+
+        //Act - Do login
+        const loginPage = new LoginPage(page);
+        await allure.step("Step 1 - Go to Login Page", async () => {
+            await page.goto(loginPage.getURL());
+        });
+
+        await allure.step("Step 2 - Input email", async () => {
+            loginPage.inputEmail(email);
+        });
+
+        await allure.step("Step 3 - Input password", async () => {
+            loginPage.inputEmail(password);
+        });
+
+        await allure.step("Step 4 - Click Login button", async () => {
+            loginPage.clickLogin();
+        });
+
+        await page.waitForEvent('dialog');
+
+        //Assert        
+        await expect(currentDialog).toBe(expectedDialog);
+    });
+});
